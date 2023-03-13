d252b16ced54ca012ec2206ae01b81451b29add0
jihwa <mungsome@gmail.com>
2023-03-13 12:37:12 +0900

    devDependencies 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   mode: "jit",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: "class",
   theme: {
